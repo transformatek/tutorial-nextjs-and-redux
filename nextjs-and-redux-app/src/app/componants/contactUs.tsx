7e8d07fb7d8d55361461216874849cc870cfc57e
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2024-12-05 11:02:59 +0000

    add contact us component

---
+++ nextjs-and-redux-app/src/app/componants/contactUs.tsx
@@ -0,0 +1,31 @@
+
+export default function ContactUs() {
+
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <div>
+                <h2>Send Message</h2>
+
+                <input placeholder="Enter your name" ></input>
+                <br />
+                <input placeholder="Enter a valid email adress" ></input>
+                <br />
+                <textarea />
+                <br />
+                <button>SUBMIT</button>
+            </div>
+            <div>
+                <h3>Adress</h3>
+                <span>Ain temouchent , Algeria</span>
+                <br />
+                <h3>Phone</h3>
+                <span> (+213) 0661-55-77-30 </span>
+                <br />
+                <h3>Email</h3>
+                <span> contact@transformatek.dz </span>
+                <br />
+            </div>
+        </div>
+    )
+}
