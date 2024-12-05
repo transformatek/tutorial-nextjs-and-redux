0508bf8c76bd8b3dc5d202a522a74cfb24771975
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2024-12-05 14:58:04 +0000

    add colors in palette of theme

---
+++ nextjs-and-redux-app/src/styles/theme.tsx
@@ -0,0 +1,11 @@
+
+export const theme = {
+    token: {
+        colorPrimary:"#63223e",
+        colorText: "#610c31",
+        colorWhite:"#ffff",
+        colorNeutral: '#806b6b',
+        colorBg:"#692b46"
+    },
+    
+};
