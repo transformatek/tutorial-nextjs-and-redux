95830d915af8b47c7d1590ba9dea444eea01b915
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2024-12-05 14:58:35 +0000

    add style

--- nextjs-and-redux-app/src/app/page.tsx
+++ nextjs-and-redux-app/src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+import { theme } from "@/styles/theme";
 import ContactUs from "./componants/contactUs";
+
 
 
 export default function Home() {
   return (
-    <main>
-      <ContactUs/>
+    <main style={{
+      height: "100vh",
+      background: `linear-gradient(180deg, ${theme.token.colorBg} 20%, ${theme.token.colorNeutral} 80%)`
+    }} >
+      <ContactUs />
     </main>
   );
 }
