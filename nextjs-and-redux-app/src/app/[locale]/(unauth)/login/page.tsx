978dbd75a9128ba255dc62ca4656a62fd6339d0a
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2024-12-09 12:05:19 +0000

    create login page

---
+++ nextjs-and-redux-app/src/app/[locale]/(unauth)/login/page.tsx
@@ -0,0 +1,24 @@
+import { Image, Space } from "antd";
+import Title from "antd/es/typography/Title";
+
+type Props = {
+  params: { locale: string };
+};
+
+export default function LogIn({ params: { locale } }: Props) {
+
+  return (
+    <Space direction="vertical" size="middle" style={{ display: 'flex', paddingTop: 15  }} >     
+     <Title level={3} style={{padding:5}} > Work in progress</Title>
+      <div style={{display:"flex", justifyContent:"center"}}>
+      
+        <Image
+          src="/images/comming-soon.png"
+          width={500}
+          height={500}
+          alt="comming-soon"
+        />
+      </div>
+    </Space>
+    )
+}
